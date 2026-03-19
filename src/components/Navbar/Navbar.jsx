4bd5732a5f258c6ce50dd86efe8811ae8a45a922
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-03-19 23:24:48 +0530

    save

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import  logo from '../../assets/logo.png'
 import './Navbar.css'
 const Navbar = () => {
+
+  const [sticky, setSticky]= useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll',() =>{
+      window.scrollY > 50 ?setSticky(true) : setSticky(false)
+    })
+  },[]);
+
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
 
         <img src={logo} className='logo'/>
 
