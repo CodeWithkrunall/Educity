fdfc2d34945f9cf7ec13dcefe37e65eb3cc24b1a
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-03-20 12:18:27 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
+import Campus from './components/Campus/Campus'
 
 const App = () => {
   return (
@@ -16,6 +17,10 @@ const App = () => {
         <Title subTitle='Out PROGRAM' title='what we offer'/>
         <Programs/>
         <About/>
+        <Title subTitle='Galary' title='Campus photo'/>
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='what student says'/>
+
       </div>
     </div>
   )
